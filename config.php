405f758cc179a3fcc8723791b0dfeec455ddcd25
ShahriarAkib <158819471+Shahriarakib21@users.noreply.github.com>
2025-03-01 01:44:59 +0600

    Update and rename maindatabase.php to config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'db'; 
+$dbname = 'login_db';
+$username = 'user';
+$password = 'password';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
