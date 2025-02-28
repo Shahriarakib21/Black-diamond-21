0c7830e2312870af2ea5e7dc1ff8227554b746d4
ShahriarAkib <158819471+Shahriarakib21@users.noreply.github.com>
2025-03-01 01:39:00 +0600

    Create init.sql

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS login_db;
+USE login_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
